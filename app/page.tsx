06f6e241e0c9ff34d042dc5c2a71580406ac3803
maziarja <maziar.ja@gmail.com>
2025-12-29 14:59:57 -0800

    Trying to fix prerendering page not-found during vercel building

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import AllCardsPage from "@/components/allCards/AllCardsPage";
 import StudyModePage from "@/components/studyMode/StudyModePage";
 
 type PageProps = {
-  searchParams: Promise<{
-    window: "all-cards" | "study-mode";
+  searchParams?: Promise<{
+    window?: "all-cards" | "study-mode";
   }>;
 };
 
 async function Page({ searchParams }: PageProps) {
-  const { window } = await searchParams;
+  const query = await searchParams;
+  const window = query?.window;
+
   return (
     <>
       {window !== "all-cards" && <StudyModePage />}
